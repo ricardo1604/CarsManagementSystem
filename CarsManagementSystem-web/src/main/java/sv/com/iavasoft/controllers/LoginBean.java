68b354a73f86cd263b9c2ad9c7dabb1729d27f2b
ricardo <ricardo@127.0.53.53>
2015-06-23 23:50:52 -0600

    Ultimocommit antes del deadline

--- CarsManagementSystem-web/src/main/java/sv/com/iavasoft/controllers/LoginBean.java
+++ CarsManagementSystem-web/src/main/java/sv/com/iavasoft/controllers/LoginBean.java
@@ -5,7 +5,11 @@
  */
 package sv.com.iavasoft.controllers;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 
@@ -43,7 +47,11 @@ public class LoginBean implements Serializable {
     }
 
     public void validarLogin() {
-        
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("/CarsManagementSystem-web/index.sertracen");
+        } catch (IOException ex) {
+            Logger.getLogger(LoginBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
